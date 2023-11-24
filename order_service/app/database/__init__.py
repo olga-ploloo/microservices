696280e0aef3c9d4d5463b070cc7cefcfcfda384
oy <olga.kozintsava@gmail.com>
2023-11-24 17:27:22 +0300

    feat: updated project structure

--- order_service/app/database/__init__.py
+++ order_service/app/database/__init__.py
@@ -1,7 +1,2 @@
-__all__ = ('Base',
-           'Product',
-           'Order',
-           'OrderStatus')
 
-from .base import Base
-from .models import Product, Order, OrderStatus
+
---
+++ order_service/app/models/__init__.py
@@ -0,0 +1,10 @@
+__all__ = ('Base',
+           'Product',
+           'Order',
+           'OrderProductAssociation')
+
+from .base import Base
+from .product import Product
+from .order import Order
+from .order_product_association import OrderProductAssociation
+
