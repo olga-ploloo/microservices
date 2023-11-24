022e16e2fd565c6f4c2262c7fe178c4ae9e341e7
oy <olga.kozintsava@gmail.com>
2023-11-24 17:27:04 +0300

    feat: added order folder

---
+++ order_service/app/api/orders/crud.py
@@ -0,0 +1,42 @@
+from sqlalchemy import select, Result
+from sqlalchemy.orm import joinedload, selectinload
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from .schemas import ProductCreate, ProductUpdate
+from order_service.app.models import Product, Order
+
+
+# async def get_products(session: AsyncSession) -> list[Product]:
+#     stmt = select(Product).order_by(Product.id)
+#     result: Result = await session.execute(stmt)
+#     products = result.scalars().all()
+#     return list(products)
+
+async def get_orders_with_products(session: AsyncSession) -> list[Order]:
+    stmt = (select(Order)
+            .options(selectinload(Order.products), )
+            .order_by(Order.id))
+    orders = await session.scalars(stmt)
+
+    return list(orders)
+
+# async def create_product(session: AsyncSession,
+#                          new_product: ProductCreate) -> Product:
+#     product = Product(**new_product.model_dump())
+#     session.add(product)
+#     await session.commit()
+#     await session.refresh(product)
+#     return product
+#
+# async def update_product(session: AsyncSession,
+#                          product: Product,
+#                          product_update: ProductUpdate) -> Product:
+#     for name, value in product_update.model_dump(exclude_unset=True).items():
+#         setattr(product, name, value)
+#     await session.commit()
+#     return product
+#
+# async def delete_product(session: AsyncSession,
+#                          product: Product) -> None:
+#     await session.delete(product)
+#     await session.commit()
